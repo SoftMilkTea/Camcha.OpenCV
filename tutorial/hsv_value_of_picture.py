694ffbbab47a64de2d80f9df917227ad699820b2
heetop <wjdgmlxkr@gmail.com>
2017-10-23 00:46:06 +0900

    show hsv pixel value

---
+++ tutorial/hsv_value_of_picture.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+from matplotlib import pyplot as plt
+
+
+dataset_dir = os.path.abspath("/Users/heetop/Desktop/Dataset")
+
+image_path=os.path.join(dataset_dir,"images/white/bright_white_spot_1.jpg")
+
+white_image=cv2.imread(image_path)
+white_image=cv2.resize(white_image,(540,960))
+white_image=cv2.GaussianBlur(white_image,(3,3),0)
+
+hsv = cv2.cvtColor(white_image,cv2.COLOR_BGR2HSV)
+print(hsv.shape)
+
+figure = plt.figure()
+plt.subplot(1,2,1) # 1*2행렬  1번째 이미지
+plt.title("ORIGIN")
+plt.imshow(white_image)
+plt.subplot(1,2,2)
+plt.title("GRAY")
+plt.imshow(hsv)
+plt.show()
